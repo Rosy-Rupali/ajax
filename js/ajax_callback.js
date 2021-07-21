3cfb9decd74828fcd304cdedaddc0bd25af291d0
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-21 23:12:05 +0530

    added: ajax demonstration using promise object

--- js/ajax_callback.js
+++ js/ajax_callback.js
@@ -1,48 +1,49 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function makeAJAXCall(methodType, url, callback, async = true, data = null) {
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    console.log(
-      "State Changed Called. Ready State: " +
-        xhr.readyState +
-        " Status:" +
-        xhr.status
-    );
-    if (xhr.readyState == 4) {
-      //Matching all 200 Series Responses
-      if (xhr.status === 200 || xhr.status === 201) {
-        callback(xhr.responseText);
-      } else if (xhr.status >= 400) {
-        console.log("Handle 400 Client Error or 500 Server Error");
-      }
-    }
-  };
-
-  xhr.open(methodType, url, async);
-  if (data) {
-    console.log(JSON.stringify(data));
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(data));
-  } else xhr.send();
-  console.log(methodType + " request sent to the server");
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+          console.log(
+            "State Changed Called. Ready State: " +
+              xhr.readyState +
+              " Status:" +
+              xhr.status
+          );
+          if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+            resolve(xhr.responseText);
+            } else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+              console.log("XHR Failed");
+            }
+          }
+      
+        xhr.open(methodType, url, async);
+        if (data) {
+          console.log(JSON.stringify(data));
+          xhr.setRequestHeader("Content-Type", "application/json");
+          xhr.send(JSON.stringify(data));
+        } else xhr.send();
+        console.log(methodType + " request sent to the server");
+    });
 }
 const getURL = "http://localhost:3000/employees/1";
-function getUserDetails(data){
-    console.log("Get User Data: "+data)
-}
-makeAJAXCall("GET", getURL, getUserDetails);
-const deleteURL = "http://localhost:3000/employees/4";
-function userDeleted(data){
-    console.log("User Deleted "+data)
-}
+makePromiseCall("GET", getURL, true).then(responseText => {
+    console.log("Get User Data: "+responseText)
+})
+.catch(error => console.log("GET Error Status: "+JSON.stringify(error)));
 
-makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+const deleteURL = "http://localhost:3000/employees/4";
+makePromiseCall("DELETE", deleteURL, false).then(responseText => {
+  console.log("User Deleted: "+responseText)
+})
+.catch(error => console.log("DELETE Error Status: "+JSON.stringify(error)));
 
 const postURL = "http://localhost:3000/employees";
 const empData = {"name": "Harry", "salary": "5000"};
-function userAdded(data){
-    console.log("User Added: "+data)
-}
-
-makeAJAXCall("POST", postURL, userAdded, true, empData);
-
+makePromiseCall("POST", postURL, true, empData).then(responseText => {
+  console.log("User Added: "+responseText)
+})
+.catch(error => console.log("POST Error Status: "+JSON.stringify(error)));
