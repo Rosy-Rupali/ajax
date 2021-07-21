0af6cf8abdeb73811c472b82af0bbaab5389ff5e
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-21 19:09:44 +0530

    added: file to demonstrate the aysnchronous nature of javascript

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,17 @@
 console.log("Ajax- Async, Callback & Promise");
+function showTime(){
+    const date = new Date();
+    return date.getHours()+"Hrs: "+date.getMinutes()+"Mins: "+date.getSeconds()+"Secs";
+}
+
+function showSessionExpire(){
+    console.log("Activity-B: Your session expired at "+showTime());
+}
+
+console.log("Activity-A: Trigerring Activity-B at "+showTime());
+/**
+ * called the callback method setTimeout which will display showSessionExpire() exactly 
+ * because of asynchronous nature after 5 seconds
+ */
+setTimeout(showSessionExpire, 5000);
+console.log("Actiivity-A: Triggered Activity-B at "+showTime()+" will execute after 5 seconds");
